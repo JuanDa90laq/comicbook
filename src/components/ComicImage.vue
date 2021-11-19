78d5feac296271f55e4e285cbbfb10da52eb66ab
JuanLara6Connex <82596982+JuanLara6Connex@users.noreply.github.com>
2021-11-19 14:59:02 -0500

    Commit inicial

---
+++ src/components/ComicImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-center font-weight-bold">{{ title }}</h1>
+    <v-img :src="image" :alt="alt"> </v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComicImage",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    alt: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters class="mt-4">
+      <v-col class="d-flex justify-center">
+        <v-skeleton-loader
+          v-if="overlay"
+          class="mx-auto"
+          max-width="300"
+          width="300"
+          type="card"
+        ></v-skeleton-loader>
+        <v-hover v-else>
+          <template v-slot:default="{ hover }">
+            <ComicImage
+              transition="fab-transition"
+              :elevation="hover ? 24 : 6"
+              :title="comic.title"
+              :image="comic.img"
+              :alt="comic.alt"
+            />
+          </template>
+        </v-hover>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="mt-4">
+      <v-col class="d-flex justify-center">
+        <v-rating
+          v-model="rating"
+          background-color="primary"
+          color="primary"
+          size="64"
+        ></v-rating>
+      </v-col>
+    </v-row>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld";
+import ComicImage from "@/components/ComicImage";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    ComicImage,
+  },
+  data() {
+    return {
+      rating: null,
+      comic: {
+        title: "",
+        img: "",
+        alt: "",
+      },
+      overlay: false,
+    };
+  },
+  async created() {
+    this.overlay = true;
+    const axiosInstance = axios.create({
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+      },
+    });
+    let comicRamdom = Math.floor(Math.random() * 100);
+    let response = await axiosInstance.get(
+      `https://localhost:8082/api/comic?num=${comicRamdom}`
+    );
+    this.comic = await response.data;
+    this.overlay = false;
+  },
+};
 </script>
